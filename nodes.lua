18fa77d0e19989f094582a31875cd9a2ba72d4a1
Jordan Irwin <antumdeluge@gmail.com>
2021-07-21 19:40:17 -0700

    Use sounds mod for node sounds

--- nodes.lua
+++ nodes.lua
@@ -4,6 +4,9 @@ local S = core.get_translator(mineral.modname)
 
 -- FIXME: depends on sounds & textures from default
 local stone_texture = "default_stone.png"
+local snd = {
+	stone = sounds.node_stone(),
+}
 
 local nodes = {
 	{
@@ -12,7 +15,7 @@ local nodes = {
 		tiles       = {stone_texture .. "^mineral_overlay_coal.png"},
 		groups      = {cracky = 3},
 		drop        = "mineral:coal_lump",
-		sounds      = default.node_sound_stone_defaults(),
+		sounds      = snd.stone,
 	},
 	{
 		name        = "stone_with_copper",
@@ -20,7 +23,7 @@ local nodes = {
 		tiles       = {stone_texture .. "^mineral_overlay_copper.png"},
 		groups      = {cracky = 2},
 		drop        = "mineral:copper_lump",
-		sounds      = default.node_sound_stone_defaults(),
+		sounds      = snd.stone,
 	},
 	{
 		name        = "stone_with_diamond",
@@ -28,7 +31,7 @@ local nodes = {
 		tiles       = {stone_texture .. "^mineral_overlay_diamond.png"},
 		groups      = {cracky = 1},
 		drop        = "mineral:diamond",
-		sounds      = default.node_sound_stone_defaults(),
+		sounds      = snd.stone,
 	},
 	{
 		name        = "stone_with_gold",
@@ -36,7 +39,7 @@ local nodes = {
 		tiles       = {stone_texture .. "^mineral_overlay_gold.png"},
 		groups      = {cracky = 2},
 		drop        = "mineral:gold_lump",
-		sounds      = default.node_sound_stone_defaults(),
+		sounds      = snd.stone,
 	},
 	{
 		name        = "stone_with_iron",
@@ -44,7 +47,7 @@ local nodes = {
 		tiles       = {stone_texture .. "^mineral_overlay_iron.png"},
 		groups      = {cracky = 2},
 		drop        = "mineral:iron_lump",
-		sounds      = default.node_sound_stone_defaults(),
+		sounds      = snd.stone,
 	},
 	{
 		name        = "stone_with_tin",
@@ -52,7 +55,7 @@ local nodes = {
 		tiles       = {stone_texture .. "^mineral_overlay_tin.png"},
 		groups      = {cracky = 2},
 		drop        = "mineral:tin_lump",
-		sounds      = default.node_sound_stone_defaults(),
+		sounds      = snd.stone,
 	},
 }
 
