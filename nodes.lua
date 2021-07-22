3a89f16526f4150388d73a7c06519104a6223be9
Jordan Irwin <antumdeluge@gmail.com>
2021-07-21 19:27:21 -0700

    Store base stone texture

--- nodes.lua
+++ nodes.lua
@@ -3,12 +3,13 @@ local S = core.get_translator(mineral.modname)
 
 
 -- FIXME: depends on sounds & textures from default
+local stone_texture = "default_stone.png"
 
 local nodes = {
 	{
 		name        = "stone_with_coal",
 		description = S("Coal Ore"),
-		tiles       = {"default_stone.png^mineral_overlay_coal.png"},
+		tiles       = {stone_texture .. "^mineral_overlay_coal.png"},
 		groups      = {cracky = 3},
 		drop        = "mineral:coal_lump",
 		sounds      = default.node_sound_stone_defaults(),
@@ -16,7 +17,7 @@ local nodes = {
 	{
 		name        = "stone_with_copper",
 		description = S("Copper Ore"),
-		tiles       = {"default_stone.png^mineral_overlay_copper.png"},
+		tiles       = {stone_texture .. "^mineral_overlay_copper.png"},
 		groups      = {cracky = 2},
 		drop        = "mineral:copper_lump",
 		sounds      = default.node_sound_stone_defaults(),
@@ -24,7 +25,7 @@ local nodes = {
 	{
 		name        = "stone_with_diamond",
 		description = S("Diamond Ore"),
-		tiles       = {"default_stone.png^mineral_overlay_diamond.png"},
+		tiles       = {stone_texture .. "^mineral_overlay_diamond.png"},
 		groups      = {cracky = 1},
 		drop        = "mineral:diamond",
 		sounds      = default.node_sound_stone_defaults(),
@@ -32,7 +33,7 @@ local nodes = {
 	{
 		name        = "stone_with_gold",
 		description = S("Gold Ore"),
-		tiles       = {"default_stone.png^mineral_overlay_gold.png"},
+		tiles       = {stone_texture .. "^mineral_overlay_gold.png"},
 		groups      = {cracky = 2},
 		drop        = "mineral:gold_lump",
 		sounds      = default.node_sound_stone_defaults(),
@@ -40,7 +41,7 @@ local nodes = {
 	{
 		name = "stone_with_iron",
 		description = S("Iron Ore"),
-		tiles = {"default_stone.png^mineral_overlay_iron.png"},
+		tiles = {stone_texture .. "^mineral_overlay_iron.png"},
 		groups = {cracky = 2},
 		drop = "mineral:iron_lump",
 		sounds = default.node_sound_stone_defaults(),
@@ -48,7 +49,7 @@ local nodes = {
 	{
 		name = "stone_with_tin",
 		description = S("Tin Ore"),
-		tiles = {"default_stone.png^mineral_overlay_tin.png"},
+		tiles = {stone_texture .. "^mineral_overlay_tin.png"},
 		groups = {cracky = 2},
 		drop = "mineral:tin_lump",
 		sounds = default.node_sound_stone_defaults(),
